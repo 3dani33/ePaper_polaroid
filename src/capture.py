c511dff4c55a22e6e17d6ac048682559c09ad052
Daniel Raab <daniel.raab@mailbox.org>
2022-02-06 10:09:23 +0100

    fix pictures too dark

--- src/capture.py
+++ src/capture.py
@@ -3,7 +3,9 @@ import cv2
 
 def get_image():
     cam = cv2.VideoCapture(0)
-    _, frame = cam.read()
+    # read image multiple times to allow camera to adjust exposure
+    for i in range(10):
+        _, frame = cam.read()
     return frame
 
 if __name__ == '__main__':
